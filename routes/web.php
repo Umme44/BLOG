c83b83352098bb19ddddbe2df092d2cc00e8468c
umme.habiba <umme.habiba@bracits.com>
2026-02-17 11:53:36 +0600

    create routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/home', function(){
+//     return view('home');
+// });
+
+Route::view('/home', 'home');
+
+//Dynamic Route
+Route::get("/about/{name}", function($name){
+  
+    return view('about', ['name'=>$name]);
+});
+
+
